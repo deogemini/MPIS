dc6c0fd8c738cad06dd3c56b5940cc1d9576596e
deogemini <51180096+deogemini@users.noreply.github.com>
2021-02-04 13:36:15 +0300

    js plugins added

--- Admin/layouts/common_base.php
+++ Admin/layouts/common_base.php
@@ -3,12 +3,19 @@
 
 <?php include 'layouts/footer.php' ?>
 <script src="/MPIS/Assets/js/jquery.js"></script>
+<script src="/MPIS/Assets/js/jquery.dataTables.min.js"></script>
+<script src="/MPIS/Assets/js/dataTables.bootstrap.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 <script  src="/MPIS/Assets/js/bootstrap.min.js"></script>
 <script src="/MPIS/Assets/plugins/dist/js/select2.min.js"></script>
 <script>
     $(function(){
       $('.js-example-basic-single').select2();
     });
+
+    $(document).ready( function () {
+    $('#myTable').DataTable();
+} );
 </script>
 </body>
 </html>
---
+++ Admin/onyesha_jimbo.php
@@ -0,0 +1,20 @@
+<?php include 'layouts/main.php';?>
+<div class="container" id="viewjimbo">  
+<h2 style="text-decoration: all;">Majimbo yaliyosajiliwa</h2>
+<table class="display">
+<tr>
+<th>Jina la jimbo</th>
+<br>
+<th>mkoa lilipo</th>
+<br>
+<th>wilaya lilipo</th>
+<br>
+<th>idadi ya kata</th>
+</tr>
+</table>
+</div>
+
+
+<!--/ Main content End Here-->
+
+<?php include 'layouts/common_base.php' ?>
