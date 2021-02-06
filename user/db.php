99725e555d1afea8e024cba3b303f0a3f872923b
deogemini <51180096+deogemini@users.noreply.github.com>
2021-02-06 14:00:23 +0300

    interface login and registration done

---
+++ user/db.php
@@ -0,0 +1,19 @@
+<?php
+$host = 'localhost';
+$user = 'root';
+$password = '';
+$dbname = 'mpis';
+
+
+try {
+    
+//Set DSN (Data source name)
+$dsn = 'mysql:host='.$host .';dbname='.$dbname;
+
+//create a PDO instance
+$pdo = new PDO($dsn, $user, $password);
+
+}catch(PDOException $e){
+    echo "DB Connection Failed:".$e->getMessage();
+}
+$pdo -> setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
--- user/login.php
+++ user/login.php
@@ -1,97 +1,109 @@
-<?php  include 'layouts/main.php';?>
+<?php include 'layouts/main.php'; ?>
 
 <style>
-body {font-family: Arial, Helvetica, sans-serif;}
-form {border: 3px solid #f1f1f1;}
+    body {
+        font-family: Arial, Helvetica, sans-serif;
+    }
 
-input[type=text], input[type=password] {
-  width: 100%;
-  padding: 12px 20px;
-  margin: 8px 0;
-  display: inline-block;
-  border: 1px solid #ccc;
-  box-sizing: border-box;
-}
+    form {
+        border: 3px;
+    }
 
-button {
-  background-color: #4CAF50;
-  color: white;
-  padding: 14px 20px;
-  margin: 8px 0;
-  border: none;
-  cursor: pointer;
-  width: 100%;
-}
+    input[type=text],
+    input[type=password] {
+        width: 100%;
+        padding: 12px 20px;
+        margin: 8px 0;
+        display: inline-block;
+        border: 1px solid #ccc;
+        box-sizing: border-box;
+    }
 
-button:hover {
-  opacity: 0.8;
-}
+    button {
+        background-color: #4CAF50;
+        color: white;
+        padding: 14px 20px;
+        margin: 8px 0;
+        border: none;
+        cursor: pointer;
+        width: 100%;
+    }
 
-.cancelbtn {
-  width:150px;
-  background-color: #f44336;
-  margin-top:-20px;
-  margin-left:50px;
-  
-}
+    button:hover {
+        opacity: 0.8;
+    }
+
+    .cancelbtn {
+        width: 140px;
+        background-color: #f44336;
+        margin-top: -20px;
+        margin-left: 50px;
+
+    }
 
 
-.container {
-  padding: 30px;
-}
+    .container {
+        padding: 30px;
+    }
 
-.container2{
-    padding: 50px;
-    margin-left:200px;
-    margin-right:200px;
-}
-span.psw {
-  float: right;
-  margin-right:100px;
+    .container2 {
+        padding: 30px;
+        margin-left: 200px;
+        margin-right: 200px;
+    }
 
-}
+    span.psw {
+        float: right;
+        margin-right: 100px;
 
-/* Change styles for span and cancel button on extra small screens */
-@media screen and (max-width: 300px) {
-  span.psw {
-     display: block;
-     float: none;
-  }
-  .cancelbtn {
-      padding-top:10px;
-     
-  }
-}
+    }
+
+    /* Change styles for span and cancel button on extra small screens */
+    @media screen and (max-width: 300px) {
+        span.psw {
+            display: block;
+            float: none;
+        }
+
+        .cancelbtn {
+            padding-top: 10px;
+
+        }
+    }
 </style>
 </head>
+
 <body>
 
-<h2>Please login in MPIS</h2>
+    <h2 class="form-text">Please login in MPIS</h2>
+    <div class="container">
+        <form action="signin.php" method="POST">
+            <div class="form-group">
+                <label for="uname"><b>First name</b></label>
+                <input class="form-control" type="text" placeholder="Enter Username" name="firstname" required>
+            </div>
 
-<form action="/action_page.php" method="post">
-  
-  <div class="container">
-    <label for="uname"><b>Username</b></label>
-    <input type="text" placeholder="Enter Username" name="uname" required>
+            <div class="form-group">
+                <label for="psw"><b>Password</b></label>
+                <input class="form-control" type="password" placeholder="Enter Password" name="psw" required>
+            </div>
 
-    <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" required>
-        
-    <button type="submit">Login</button>
-    <label>
-      <input type="checkbox" checked="checked" name="remember"> Remember me
-    </label>
-  </div>
+            <button type="submit">Login</button>
+            <label>
+                <input type="checkbox" checked="checked" name="remember"> Remember me
+            </label>
 
-  <div class="container2" style="background-color:#f1f1f1">
-    <button type="button" class="cancelbtn">Cancel</button>
-    <span class="psw">Forgot <a href="#">password?</a></span>
-  </div>
 
-</form>
+            <div class="container2">
+                <button type="button" class="cancelbtn">Cancel</button>
+                <span class="psw">Forgot <a href="#">password?</a></span>
+            </div>
 
+        </form>
+    </div>
 </body>
+
 </html>
 
 
-<?php include 'layouts/common_base.php';?>
+<?php include 'layouts/common_base.php'; ?>
