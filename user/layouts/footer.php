a352f2968b64698b40fb2039a456feeccd559c60
deogemini <51180096+deogemini@users.noreply.github.com>
2021-02-08 04:32:10 +0300

    compliment component added

--- user/layouts/footer.php
+++ user/layouts/footer.php
@@ -1,11 +1,11 @@
 <!--  footer-->
 <div class="fixed-bottom">
-	
 
-<footer class="footer"  >
-      <div class="container py-1">
-      	<span class="text-dark"> <strong>MPIS </strong></strong>@ 2021 Copyright</span>
-      </div>
-    </footer>
+
+  <footer class="footer">
+    <div class="container py-1">
+      <span class="text-dark"> <strong>MPIS </strong></strong>@ 2021 Copyright</span>
+    </div>
+  </footer>
 
 </div>
