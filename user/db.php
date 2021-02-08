a64cd97d603614837c8f6cea9fb048ce8817633c
deogemini <51180096+deogemini@users.noreply.github.com>
2021-02-08 03:06:13 +0300

    citizen's province displaying when logged in

--- user/db.php
+++ user/db.php
@@ -17,3 +17,5 @@ $pdo = new PDO($dsn, $user, $password);
     echo "DB Connection Failed:".$e->getMessage();
 }
 $pdo -> setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
--- user/index2.php
+++ user/index2.php
@@ -1,24 +1,33 @@
-<?php include 'layouts/main.php';?>
-<?php 
+<?php include 'layouts/main.php'; ?>
+<?php
 session_start();
-if(isset($_SESSION["firstname"])){
-echo '<h3>Login Success, Welcome.. '.$_SESSION["firstname"].'</h3>';
-}
-else{
+if (isset($_SESSION["firstname"])) {
+    echo '<p>Login Success, Welcome.. ' . $_SESSION["firstname"] . '</p>';
+} else {
     header("location:login.php");
 }
 ?>
 <div class="container">
-<div class="row">
-<div class="col-md-10">
-<h2> Welcome at....</h2>
-</div>
-<div class="col-md-2">
-<form action="logout.php">
-<button type="submit" class="btn btn-danger" name="logout">Logout here</button>
-</form>
-</div>
-</div>
+    <div class="row">
+        <div class="col-md-10">
+            <?php
+            include 'db.php';
+            if (isset($_SESSION["firstname"])) {
+                $firstname = $_SESSION["firstname"];
+                $sql = "SELECT jimbo FROM citizen WHERE firstname = '$firstname'";
+                $stmt = $pdo->prepare($sql);
+                $stmt->execute(["firstname" => $firstname]);
+                while ($row = $stmt->fetch())
+                    echo '<h1> Karibu Jimbo la .... <span style="text-transform:uppercase"> <b>' . $row->jimbo . "</span></b></h1>";
+            }
+            ?>
+        </div>
+        <div class="col-md-2">
+            <form action="logout.php">
+                <button type="submit" class="btn btn-danger" name="logout">Logout here</button>
+            </form>
+        </div>
+    </div>
 </div>
 
 <?php include 'layouts/common_base.php'; ?>
