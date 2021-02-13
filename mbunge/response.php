8fd3e90679cbfc13f2663a0cce1e1bc4ba2eb3b9
deogemini <51180096+deogemini@users.noreply.github.com>
2021-02-13 20:28:01 +0300

    response updating table post

--- mbunge/response.php
+++ mbunge/response.php
@@ -2,17 +2,21 @@
 
 include 'db.php';
 if (isset($_POST['submit'])) {
-
+        $id = $_GET['id'];
     $response = $_POST['response'];
-    $idd = $_GET['id'];
-    die($idd);
 
- if (empty($response)){
-     $status="All fields must be filled";
+//  if (empty($response)){
+//      $status="All fields must be filled";
      
- }else{
-     $sql= "UPDATE response FROM post WHERE id=$id";
+//  }else{
+     $sql= 'UPDATE post SET response=:response WHERE id=:id';
+       $stmt = $pdo->prepare($sql);
+        $stmt->execute(["response" => $response, "id"=>$id]);
+                header("Location: view_comments.php");
+
+
+     
 
  }
-}
+
 ?>
