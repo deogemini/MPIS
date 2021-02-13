eb689a04dc205b943945bcdacb65d1dfd025aa08
deogemini <51180096+deogemini@users.noreply.github.com>
2021-02-13 10:28:46 +0300

    mbunge table template added

--- mbunge/mbunge_profile.php
+++ mbunge/mbunge_profile.php
@@ -1,5 +1,51 @@
-<?php include 'layouts/main.php';?>
+<?php include 'layouts/main.php'; ?>
+<div class="container">
+    <h3>Mbunge details</h3>
+    <table class="table table-bordered">
+        <tr>
+            <td><b>First Name</b></td>
+            <td>Joe Boy</td>
+        </tr>
+        <tr>
+            <td><b>Second Name</b></td>
+            <td>Zuchu</td>
+        </tr>
+        <tr>
+            <td><b>Last Name</b></td>
+            <td>Mkali wenu</td>
+        </tr>
+        <tr>
+            <td><b>Gender</b></td>
+            <td>Male</td>
+        </tr>
+        <tr>
+            <td><b>Birthdate</b></td>
+            <td>1999-04-03</td>
+        </tr>
+        <tr>
+            <td><b>Email</b></td>
+            <td>mwajungwa@gmail.com</td>
+        </tr>
+        <tr>
+            <td><b>Starting date</b></td>
+            <td>2020-04-03</td>
+        </tr>
+        <tr>
+            <td><b>Finishing date</b></td>
+            <td>2025-04-03</td>
+        </tr>
+        <tr>
+            <td><b>Jimbo</b></td>
+            <td>Arumeru</td>
+        </tr>
+        <tr>
+            <td><b>Chama</b></td>
+            <td>CHAMA CHA MAPINDUZI</td>
+        </tr>
 
+
+    </table>
+</div>
 
 
 
