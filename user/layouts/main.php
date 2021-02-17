09124ae4e92d27d9c2c956b1938283dca061222b
Sharifu Kitori <51135024+kitori-kitori@users.noreply.github.com>
2021-02-17 11:52:58 +0300

    Update main.php

--- user/layouts/main.php
+++ user/layouts/main.php
@@ -5,7 +5,6 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>MainPage</title>
-
   <link rel="stylesheet" href="/MPIS/Assets/css/bootstrap.min.css">
   <link rel="stylesheet" href="/MPIS/Assets/css/custom.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
