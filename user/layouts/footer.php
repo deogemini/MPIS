4bd7666ee1444fdcdb867b81f2e4ed306061b266
Sharifu Kitori <51135024+kitori-kitori@users.noreply.github.com>
2021-02-17 11:50:22 +0300

    Update footer.php

--- user/layouts/footer.php
+++ user/layouts/footer.php
@@ -2,7 +2,7 @@
 <div class="fixed-bottom">
   <footer class="footer">
     <div class="container py-1">
-      <span class="text-dark"> <strong>MPIS </strong></strong>@ 2021 Copyright©</span>
+      <span class="text-dark"> <strong>MPIS </strong>@ 2021 Copyright©</span>
     </div>
   </footer>
 </div>
