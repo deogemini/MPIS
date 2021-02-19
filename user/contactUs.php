ef166d15d81d04c444547d3bd2f5fd998052210b
deogemini <51180096+deogemini@users.noreply.github.com>
2021-02-19 13:34:06 +0300

    linking contact page with home page

--- user/contactUs.php
+++ user/contactUs.php
@@ -1,70 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-body {
-  font-family: Arial, Helvetica, sans-serif;
-}
+<?php include 'layouts/main.php'; ?>
 
-* {
-  box-sizing: border-box;
-}
-
-/* Style inputs */
-input[type=text],input[type=email], select, textarea {
-  width: 100%;
-  padding: 12px;
-  border: 1px solid #ccc;
-  margin-top: 6px;
-  margin-bottom: 16px;
-  resize: vertical;
-}
-
-input[type=submit] {
-  background-color: #4CAF50;
-  color: white;
-  padding: 12px 20px;
-  border: none;
-  cursor: pointer;
-}
-
-input[type=submit]:hover {
-  background-color: #45a049;
-}
-
-/* Style the container/contact section */
-.container {
-  border-radius: 5px;
-  background-color: #f2f2f2;
-  padding: 10px;
-}
-
-/* Create two columns that float next to eachother */
-.column {
-  float: left;
-  width: 50%;
-  margin-top: 6px;
-  padding: 20px;
-}
-
-/* Clear floats after the columns */
-.row:after {
-  content: "";
-  display: table;
-  clear: both;
-}
-
-/* Responsive layout - when the screen is less than 600px wide, make the two columns stack on top of each other instead of next to each other */
-@media screen and (max-width: 600px) {
-  .column, input[type=submit] {
-    width: 100%;
-    margin-top: 0;
-  }
-}
-</style>
-</head>
-<body>
 <div class="container">
   <div style="text-align:center">
     <h2>Contact Us</h2>
@@ -72,17 +7,17 @@ input[type=submit]:hover {
   </div>
   <div class="row">
     <div class="column">
-     Phone Call<br><br>
-    Project Manager <br><br>
-    +25523455464566<br><br>
-    +255713664385<br><br>
+      Phone Call<br><br>
+      Project Manager <br><br>
+      +25523455464566<br><br>
+      +255713664385<br><br>
 
-    Email: Deogmin@gmail.com<br><br><br>
+      Email: Deogmin@gmail.com<br><br><br>
 
-    Postal address<br><br>
-    University of Dar es Salaam<br>
-    p.o.box 305<br>
-    Dar es Salaam<br><br>
+      Postal address<br><br>
+      University of Dar es Salaam<br>
+      p.o.box 305<br>
+      Dar es Salaam<br><br>
     </div>
     <div class="column">
       <form action="/action_page.php">
@@ -100,6 +35,3 @@ input[type=submit]:hover {
     </div>
   </div>
 </div>
-
-</body>
-</html>
