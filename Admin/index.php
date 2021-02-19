1fcc997bd0638859fb0aaabcd01bee1e77f71c89
sarahmnadi <sarahmnadi2020@gmail.com>
2021-02-19 18:45:29 +0300

    editing admin index page

--- Admin/index.php
+++ Admin/index.php
@@ -9,31 +9,16 @@
 	?>
 	<p class="text-justify">
 
-	<h4> <b>Welcome to MPIS</b> </h4>
+	<h4> <b>Admin...Welcome to MPIS</b> </h4>
 
-	Member of parliament information system (MPIS), it's the platform that olds Citizens compliments and Members of parliamnets details and feedback to the respective compliment.<br><br>
+	Member of parliament information system (MPIS), it's the platform that holds Citizens compliments and Members of parliamnets details and feedback to the respective compliment.<br><br>
 		
 <h5><b> What does MPIS do? </b></h5> 
 
-MPIS allows Member of parliamnet and citizens of Tanzania to solve challenges that cictizens are facing online. 
-Here is an example of what MPIS can do:<br><br>
+MPIS allows admin to: <br> Register Member of parliament and their constituency,:<br>
 
-<h6>Member of parliamnet</h6>
-View citizens compliments<br>
-provides feedback to citizens<br><br>
-
-<h6>citizens</h6>
-
-provides compliments to Members of parliaments<br>
-Views feedback<br>
-Views complimnets status<br><br>
-
-<h6>Other</h6>
-register Members of parliament<br>
-Register constituency<br>
-view list of Members of parliament<br>
-view list of constituency
-
+ View registered Member of parliaments with their respective constituency
+<hr>
 
 	</p>
 </div>
