810174bb3b65236f000e1118b237c7878ff04642
deogemini <51180096+deogemini@users.noreply.github.com>
2021-03-28 23:47:40 +0300

    heroku dbconnection.php added

---
+++ dbconnection.php
@@ -0,0 +1,11 @@
+<?php
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
