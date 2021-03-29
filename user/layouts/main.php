8db3804542e35e606ffa4e8e6c27a56f266c3447
deogemini <51180096+deogemini@users.noreply.github.com>
2021-03-29 12:17:35 +0300

    changes of path in main index

--- user/layouts/main.php
+++ user/layouts/main.php
@@ -33,7 +33,7 @@
       <div class="collapse navbar-collapse" id="navbarNavDropdown">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item ">
-            <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="/index.php">About us</a>
