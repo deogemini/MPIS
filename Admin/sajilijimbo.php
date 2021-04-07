3ccf19980d4aa770c15f8c5047964b373d5a3109
deogemini <51180096+deogemini@users.noreply.github.com>
2021-04-07 06:32:17 +0300

    data entry for constituncies

--- Admin/sajilijimbo.php
+++ Admin/sajilijimbo.php
@@ -1,31 +1,28 @@
-<?php 
+<?php
 
-include ('db.php');
+include('db.php');
 
 // //  INSERT DATA
 $status  = "";
-if(isset($_POST['submit'])){
+if (isset($_POST['submit'])) {
     $jinalajimbo = $_POST['jinalajimbo'];
     $mkoa = $_POST['mkoa'];
-    $wilaya = $_POST['wilaya'];
+    $halmashauri = $_POST['halmashauri'];
     $kata = $_POST['kata'];
 
-    if(empty($jinalajimbo) || empty($mkoa) || empty($wilaya) || empty($kata)) {
+    if (empty($jinalajimbo) || empty($mkoa) || empty($halmashauri) || empty($kata)) {
         $status = "All fields are compulsory";
     } else {
-        $sql = "INSERT INTO jimbo(jinalajimbo, mkoa, wilaya, kata) VALUES (:jinalajimbo,:mkoa, :wilaya, :kata) ";
-        $stmt = $pdo -> prepare($sql);
-        $stmt -> execute(["jinalajimbo" => $jinalajimbo, "mkoa" => $mkoa, "wilaya" => $wilaya, "kata" => $kata ]);
+        $sql = "INSERT INTO jimbo(jinalajimbo, mkoa, halmashauri, kata) VALUES (:jinalajimbo,:mkoa, :halmashauri, :kata) ";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute(["jinalajimbo" => $jinalajimbo, "mkoa" => $mkoa, "halmashauri" => $halmashauri, "kata" => $kata]);
 
-        $_SESSION['success']="Jimbo limeshasajiliwa";	
+        $_SESSION['success'] = "Jimbo limeshasajiliwa";
 
 
         header("Location: /MPIS/Admin/index.php");
         exit();
 
         $status = "Your data was sent";
-        
     }
-    
-    
 }
